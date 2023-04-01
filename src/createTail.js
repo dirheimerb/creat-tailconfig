90392c47f8759a82d9092764f3a5ef98439a62d0
Bradley Dirheimer <dirheimerb@gmail.com>
2023-04-01 17:58:29 -0400

    initial

---
+++ src/createTail.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+const tailwindConfig = `
+module.exports = {
+    darkMode: ['class', '[data-mode="dark"]'], // or 'media' or 'class'
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        // Or if using 'src' directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+  plugins: [],
+}`
+
+const tailCSS = `
+@tailwind base;
+@tailwind components;
+@tailwind utilities;`
+
+const postCSSConfig = `
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}`
+
+
+
+fs.writeFileSync('tailwindconfig.js', tailwindConfig, 'utf8')
+fs.writeFileSync('postcss.config.js', postCSSConfig, 'utf8')
+fs.writeFileSync('tailwind.css', tailCSS, 'utf8')
